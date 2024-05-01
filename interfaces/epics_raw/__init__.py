813312a277af99fd8a8cc57527571bb6de183ec9
Zhe Zhang <zhezhang@slac.stanford.edu>
2024-05-01 10:33:57 -0700

    Resolve the multi-threading crash issue in epics_raw interface

--- interfaces/epics_raw/__init__.py
+++ interfaces/epics_raw/__init__.py
@@ -11,6 +11,11 @@ class Interface(interface.Interface):
     testing: bool = False
     timeout: float = None
 
+    def __init__(self, **data):
+        super().__init__(**data)
+
+        # print(f'Interface {self.name} created.')
+
     @interface.log
     def get_values(self, channel_names, as_string: bool = False):
         channel_outputs = {}
@@ -23,8 +28,15 @@ class Interface(interface.Interface):
 
             return channel_outputs
 
-        values = epics.caget_many(channel_names, as_string=as_string,
-                                  timeout=self.timeout)
+        # Have to use pv level actions to avoid multi-threading issues
+        # https://pyepics.github.io/pyepics/advanced.html#using-python-threads
+        # Also the following code is quite fast, comparable to caget_many
+        # https://pyepics.github.io/pyepics/advanced.html#strategies-for-connecting-to-a-large-number-of-pvs
+        pvs = [epics.PV(name) for name in channel_names]
+        values = [p.get(as_string=as_string, timeout=self.timeout)
+                  for p in pvs]
+        # values = epics.caget_many(channel_names, as_string=as_string,
+        #                           timeout=self.timeout)
         for i, channel in enumerate(channel_names):
             channel_outputs[channel] = values[i]
 
@@ -43,9 +55,18 @@ class Interface(interface.Interface):
 
             return channel_flags
 
-        flags = epics.caput_many(pvlist, values, wait="all")
+        # Have to use pv level actions to avoid multi-threading issues
+        # For details take a look at the comments in get_values()
+        pvs = [epics.PV(name) for name in pvlist]
+        flags = [p.put(value, wait=True) for p, value in zip(pvs, values)]
+        # flags = epics.caput_many(pvlist, values, wait="all")
 
         for i, channel in enumerate(pvlist):
             channel_flags[channel] = flags[i]
 
         return channel_flags
+
+    def __del__(self):
+        pass
+
+        # print(f'Interface {self.name} deleted.')
