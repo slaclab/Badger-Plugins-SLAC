6c6cd569ac43883531d7030cf73fe0f032963464
Zhe Zhang <zhezhang@slac.stanford.edu>
2024-06-11 15:42:34 -0700

    Fix the epics-routine not running issue for both thread- and process-based core

--- interfaces/epics_raw/__init__.py
+++ interfaces/epics_raw/__init__.py
@@ -11,13 +11,10 @@ class Interface(interface.Interface):
     testing: bool = False
     timeout: float = None
 
-    def __init__(self, **data):
-        super().__init__(**data)
-
-        # print(f'Interface {self.name} created.')
-
     @interface.log
     def get_values(self, channel_names, as_string: bool = False):
+        epics.ca.clear_cache()
+
         channel_outputs = {}
 
         # if testing generate some random numbers and return
@@ -28,15 +25,9 @@ class Interface(interface.Interface):
 
             return channel_outputs
 
-        # Have to use pv level actions to avoid multi-threading issues
-        # https://pyepics.github.io/pyepics/advanced.html#using-python-threads
-        # Also the following code is quite fast, comparable to caget_many
-        # https://pyepics.github.io/pyepics/advanced.html#strategies-for-connecting-to-a-large-number-of-pvs
-        pvs = [epics.PV(name) for name in channel_names]
-        values = [p.get(as_string=as_string, timeout=self.timeout)
-                  for p in pvs]
-        # values = epics.caget_many(channel_names, as_string=as_string,
-        #                           timeout=self.timeout)
+        values = epics.caget_many(
+            channel_names, as_string=as_string, timeout=self.timeout
+        )
         for i, channel in enumerate(channel_names):
             channel_outputs[channel] = values[i]
 
@@ -44,6 +35,8 @@ class Interface(interface.Interface):
 
     @interface.log
     def set_values(self, channel_inputs: dict) -> dict:
+        epics.ca.clear_cache()
+
         channel_flags = {}
 
         pvlist = list(channel_inputs.keys())
@@ -55,18 +48,9 @@ class Interface(interface.Interface):
 
             return channel_flags
 
-        # Have to use pv level actions to avoid multi-threading issues
-        # For details take a look at the comments in get_values()
-        pvs = [epics.PV(name) for name in pvlist]
-        flags = [p.put(value, wait=True) for p, value in zip(pvs, values)]
-        # flags = epics.caput_many(pvlist, values, wait="all")
+        flags = epics.caput_many(pvlist, values, wait="all")
 
         for i, channel in enumerate(pvlist):
             channel_flags[channel] = flags[i]
 
         return channel_flags
-
-    def __del__(self):
-        pass
-
-        # print(f'Interface {self.name} deleted.')
